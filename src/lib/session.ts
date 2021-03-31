46b7fde2b9bfe75d902792ad6e86db4c38234f53
pixelmund <pixelmund@gmail.com>
2021-03-31 20:49:01 +0200

    Update client session handler

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,28 +1,45 @@
-import { session as sessionStore } from '$app/stores';
-import type { Writable } from 'svelte/store';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { onDestroy } from 'svelte';
+import { writable } from 'svelte/store';
+import type { Readable, Writable } from 'svelte/store';
 
-type Options = {
-	optimistic?: boolean;
-};
-
-let updating: boolean = false;
+let updating = false;
 let prevSession: any = {};
 
-export function initializeSession(sess: Writable<any>) {
-	const unsubscriber = sess.subscribe((val: any) => {
-		if (!updating) {
-			prevSession = val;
-		}
-	});
-	return unsubscriber;
-}
+let options = {
+	endpoint: '/--session--',
+	optimistic: true
+};
 
-async function handleSession(method: 'POST' | 'PUT' | 'DELETE', value: any, opts: Options) {
+type HandleSession = (
+	method: 'POST' | 'PUT' | 'DELETE',
+	value: any,
+	opts: { optimistic?: boolean },
+	sessionStore: Writable<any>
+) => Promise<void>;
+
+type SessionFn = (value?: any, opts?: { optimistic?: boolean }) => void;
+
+export let session: {
+	set: SessionFn;
+	refresh: SessionFn;
+	destroy: SessionFn;
+	subscribe: Readable<any>['subscribe'];
+} = {
+	subscribe: writable<any>({}).subscribe,
+	set: (val, opts) => {},
+	refresh: (val, opts) => {},
+	destroy: (val, opts) => {}
+};
+
+const handleSession: HandleSession = async function (method, value, opts, sessionStore) {
 	updating = true;
 	if (opts.optimistic) {
 		sessionStore.set(value);
 	}
-	const response = await fetch('/--session--', {
+	const response = await fetch(options.endpoint, {
 		method,
 		body: method !== 'DELETE' ? JSON.stringify(value) : undefined,
 		credentials: 'include',
@@ -40,18 +57,41 @@ async function handleSession(method: 'POST' | 'PUT' | 'DELETE', value: any, opts
 	}
 	const data = await response.json();
 	updating = false;
+	prevSession = data;
 	sessionStore.set(method === 'DELETE' ? {} : data);
-}
+};
 
-export const session = {
-	set: (value: any, opts: Options = { optimistic: false }) => {
-		handleSession('POST', value, opts);
-	},
-	refresh: (value: any, opts: Options = { optimistic: false }) => {
-		handleSession('PUT', value, opts);
-	},
-	destroy: (opts: Options = { optimistic: false }) => {
-		handleSession('DELETE', null, opts);
-	},
-	subscribe: sessionStore.subscribe
-} as const;
+export function initializeSession(
+	sessionStore: Writable<any>,
+	opts?: { optimistic?: boolean; endpoint?: string }
+): void {
+	if (opts) {
+		options = {
+			...options,
+			...opts
+		};
+	}
+
+	const unsubscriber = sessionStore.subscribe((val: any) => {
+		if (!updating) {
+			prevSession = val;
+		}
+	});
+
+	session = {
+		set: (value: any, opts = { optimistic: options.optimistic }) => {
+			handleSession('POST', value, opts, sessionStore);
+		},
+		refresh: (value: any, opts = { optimistic: options.optimistic }) => {
+			handleSession('PUT', value, opts, sessionStore);
+		},
+		destroy: (opts = { optimistic: options.optimistic }) => {
+			handleSession('DELETE', null, opts, sessionStore);
+		},
+		subscribe: sessionStore.subscribe
+	} as const;
+
+	onDestroy(() => {
+		unsubscriber();
+	});
+}
