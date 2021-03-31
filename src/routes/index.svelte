b7f4d5297549b8d7936aa2f3e369432d56467dce
pixelmund <pixelmund@gmail.com>
2021-03-31 21:01:19 +0200

    Update Example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 	}
 	async function refreshSession() {
 		try {
-			session.refresh({ username }, { optimistic: true });
+			session.refresh({ username });
 		} catch (error) {
 			console.log(error);
 		}
@@ -25,24 +25,46 @@
 	}
 </script>
 
-<main>
-	<h1>Svelte Kit Session Example</h1>
-	<p>Click on the Set Session Button to Set the session</p>
+<main
+	class="flex bg-gray-100 flex-col items-center justify-center min-h-screen w-full overflow-y-auto"
+>
+	<div class="max-w-4xl p-8 bg-white rounded-lg">
+		<h1 class="text-4xl text-gray-800 text-center font-semibold">Svelte Kit Session Example</h1>
+		<p class="text-center text-gray-400 font-light mt-2">
+			Click on the Set Session Button to Set the session
+		</p>
 
-	<h3>Session:</h3>
-	<div>
-		<button on:click={destroySession}>Destroy Session</button>
-		<button on:click={refreshSession}>Refresh Session</button>
-	</div>
-	<pre>
-		{JSON.stringify($session)}
-	</pre>
-
-	<div>
-		<div>
-			<label for="username">Username</label>
-			<input id="username" name="username" type="text" bind:value={username} />
+		<div class="flex justify-between items-center py-5 mt-6">
+			<button
+				class="py-2 px-5 bg-red-500 text-white font-semibold text-center rounded-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-red-400"
+				on:click={destroySession}>Destroy Session</button
+			>
+			<button
+				class="py-2 px-5 bg-indigo-500 text-white font-semibold text-center rounded-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-400"
+				on:click={refreshSession}>Refresh Session</button
+			>
 		</div>
-		<button on:click={setSession}>Set Session</button>
+
+		<pre
+			class="bg-gray-700 text-gray-400 py-4 px-3">
+			{JSON.stringify($session, null, 2)}
+		</pre>
+
+		<div>
+			<div class="mb-4 mt-5">
+				<label class="text-gray-500 text-sm block" for="username">Username</label>
+				<input
+					class="py-2 px-3 w-full border-gray-300 border rounded-md mt-1.5 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-400"
+					id="username"
+					name="username"
+					type="text"
+					bind:value={username}
+				/>
+			</div>
+			<button
+				class="py-2 px-5 w-full bg-purple-500 text-white font-semibold text-center rounded-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-purple-400"
+				on:click={setSession}>Set Session</button
+			>
+		</div>
 	</div>
 </main>
